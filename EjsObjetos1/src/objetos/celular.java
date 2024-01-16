fe247e41c587335b2891cc63d42fe76a8912faef
“Lara <larasonodelaviuda@gmail.com>
2024-01-16 08:58:57 +0100

    Ejercicio 5

---
+++ EjsObjetos1/src/objetos/celular.java
@@ -0,0 +1,14 @@
+package objetos;
+
+public class celular {
+	String modelo;
+	int almacenamiento;
+	int camaraMP;
+	
+	public celular() {
+		modelo="Genérico";
+		almacenamiento=64;
+		camaraMP=12;
+	}
+
+}
