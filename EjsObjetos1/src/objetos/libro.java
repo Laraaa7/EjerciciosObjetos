655c69f684f827a331a718569c0d4dc61ed6838d
“Lara <larasonodelaviuda@gmail.com>
2024-01-16 08:56:27 +0100

    Ejercicio 2

---
+++ EjsObjetos1/src/objetos/libro.java
@@ -0,0 +1,14 @@
+package objetos;
+
+public class libro {
+
+	String titulo;
+	String autor;
+	int numPaginas;
+
+	public libro(){
+		titulo="Sin nombre";
+		autor="Desconocido";
+		numPaginas=100;
+	}
+}
