9cb1747e8d1065dbd1c1c0c540be3950bf888b29
“Lara <larasonodelaviuda@gmail.com>
2024-01-16 08:57:14 +0100

    Ejercicio 3

---
+++ EjsObjetos1/src/objetos/bicicleta.java
@@ -0,0 +1,14 @@
+package objetos;
+
+public class bicicleta {
+
+	int numRuedas;
+	String tipo;
+	int tamaño;
+	
+	public bicicleta() {
+		numRuedas=2;
+		tipo="Montaña";
+		tamaño=26;
+	}
+}
---
+++ EjsObjetos1/src/objetos/uso_bicicleta.java
@@ -0,0 +1,12 @@
+package objetos;
+
+public class uso_bicicleta {
+
+	public static void main(String[] args) {
+		
+		bicicleta montaña = new bicicleta();
+		System.out.println("El tipo de bicicleta es de "+ montaña.tipo + " y su tamaño es de "+ montaña.tamaño);
+
+	}
+
+}
